5eb1a790a6da4d97f3fcb5544a182366efa7ff2c
Shivam <shivamguptanitw@gmail.com>
2023-12-20 22:55:17 +0530

    Find element in array exist or not

--- 1.Arrays/InputArray.java
+++ 1.Arrays/InputArray.java
@@ -39,10 +39,37 @@ public class InputArray {
         return largest;
     }
 
+    public static int linearSearchArray(int[] arr){
+        int index = -1;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter element to search in Array : ");
+        int ele = s.nextInt();
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == ele){
+                index = i;
+                return index;
+            }
+        }
+        return index;
+    }
+
+    public static boolean isElementExist(int[] arr){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter element to find in array: ");
+        int element = s.nextInt();
+        for(int i = 0; i < arr.length; i++){
+            if(element == arr[i]){
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int arr[] = takeInput();
         printArray(arr);
         System.out.println("Sum of Arrays Element: " + sumOfArraysElement(arr));
         System.out.println("Largest Element in Array : " + largestElement(arr));
+        System.out.println("Linear Search in Array : " + linearSearchArray(arr));
+        System.out.println("Find Element in Array : " + isElementExist(arr));
     }
 }
