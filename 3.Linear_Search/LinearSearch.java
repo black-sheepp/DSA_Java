6cb54ad7ca0f0d089a15091727f85375c358d450
Shivam <shivamguptanitw@gmail.com>
2023-12-23 11:21:20 +0530

    Linear search in Array

---
+++ 3.Linear_Search/LinearSearch.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Return the index if the target is found
+            }
+        }
+        return -1; // Return -1 if the target is not found
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        System.out.println(linearSearch(arr, 7));
+    }
+}
